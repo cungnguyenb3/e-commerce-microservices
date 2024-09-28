a2eeb5391eb507f712c7ef938d5e32653fbe2104
cungnv <cugngnv@d-soft.com.vn>
2024-09-29 00:09:52 +0700

    feat(cung) add feature document with open api

--- api-gateway/src/main/java/com/kevin/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/kevin/gateway/config/SecurityConfig.java
@@ -9,9 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+    "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(freeResourceUrls)
+                .permitAll()
+                .anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
--- order-service/src/main/java/com/kevin/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/kevin/microservices/order/client/InventoryClient.java
@@ -1,13 +1,10 @@
 package com.kevin.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
