6bfe5708731d6fc739b104da59296ab5813c9ae4
cungnguyenb3 <cungnguyenb3@gmail.com>
2024-09-30 00:29:14 +0700

    feat(cung) add circuit breaker with resilence4j

--- order-service/src/main/java/com/kevin/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/kevin/microservices/order/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.kevin.microservices.order.config;
 
 import com.kevin.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,9 +22,18 @@ public class RestClientConfig {
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
-                .baseUrl(inventoryServiceUrl).build();
+                .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientHttpRequestFactory())
+                .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
