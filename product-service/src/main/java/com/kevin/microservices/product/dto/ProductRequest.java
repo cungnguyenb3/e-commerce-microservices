bf8e9c481913a1deedb0fa25d46c8595bb987859
cungnguyenb3 <cungnguyenb3@gmail.com>
2024-09-30 14:10:13 +0700

    feat(cung) Add Angular app and connect to server

--- product-service/src/main/java/com/kevin/microservices/product/dto/ProductRequest.java
+++ product-service/src/main/java/com/kevin/microservices/product/dto/ProductRequest.java
@@ -2,5 +2,5 @@ package com.kevin.microservices.product.dto;
 
 import java.math.BigDecimal;
 
-public record ProductRequest(String id, String name, String description, BigDecimal price) {
+public record ProductRequest(String id, String name, String skuCode, String description, BigDecimal price) {
 }
