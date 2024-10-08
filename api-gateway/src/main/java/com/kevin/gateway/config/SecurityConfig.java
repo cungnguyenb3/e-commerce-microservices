f2f89decb0a3048e3bb5d766f65b35eb74aecf42
cungnguyenb3 <cungnguyenb3@gmail.com>
2024-10-08 14:58:19 +0700

    observability using grafana stack

--- api-gateway/src/main/java/com/kevin/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/kevin/gateway/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-    "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+    "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
